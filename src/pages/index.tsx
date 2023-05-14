c5cb2268394d0597a6ec3bd5007b225be0b2e38a
Joseph van Bohemen <joevanbo@protonmail.com>
2023-05-14 18:11:39 +0100

    buster

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,7 +31,10 @@ export interface HomeProps {
 }
 // https://github.com/dominicarrojado/react-typescript-swiper/blob/main/src/components/Swiper.tsx
 export default function Home(props: HomeProps) {
+  const containerRef = useRef<HTMLUListElement>(null);
+  const containerWidthRef = useRef(0); // can I use containerRef.current.scrollLeft instead?
   const containerOffsetXRef = useRef(0); // can I use containerRef.current.scrollLeft instead?
+  const minXRef = useRef(0);
   const startXRef = useRef(0);
 
   // useStateRef
@@ -42,18 +45,24 @@ export default function Home(props: HomeProps) {
     setOffsetX(n);
   };
 
+  const [swiping, setSwiping] = useState(false);
+
   const swipeStart = (e: SwipeEvent) => {
     const event = "changedTouches" in e ? e.changedTouches[0] : e;
 
     containerOffsetXRef.current = offsetXRef.current;
     startXRef.current = event.clientX;
+    const containerOffsetWidth = containerRef.current?.offsetWidth ?? 0;
+    const containerScrollWidth = containerRef.current?.scrollWidth ?? 0;
+    minXRef.current = containerOffsetWidth - containerScrollWidth;
+    containerWidthRef.current = containerOffsetWidth;
     // add the listener to the window, not the swipable div
     // mousemove should listen even if mouse leaves the swipable div while mouse is down
     window.addEventListener("mousemove", swipeMove);
     window.addEventListener("touchmove", swipeMove);
     window.addEventListener("mouseup", swipeEnd);
     window.addEventListener("touchend", swipeEnd);
-    // setSwiping(true);
+    setSwiping(true);
   };
   const swipeMove = (e: SwipeEvent) => {
     const event = "changedTouches" in e ? e.changedTouches[0] : e;
@@ -62,10 +71,33 @@ export default function Home(props: HomeProps) {
     if (nextOffsetX > 0) {
       nextOffsetX = 0;
     }
+    if (nextOffsetX < minXRef.current) {
+      nextOffsetX = minXRef.current;
+    }
     updateOffsetX(nextOffsetX);
   };
   const swipeEnd = (e: SwipeEvent) => {
     const event = "changedTouches" in e ? e.changedTouches[0] : e;
+
+    let nextOffsetX = offsetXRef.current;
+    const w = containerWidthRef.current;
+    const swipeDiff = startXRef.current - event.clientX;
+    if (Math.abs(swipeDiff) > 50) {
+      if (swipeDiff > 0) {
+        // swipe to the right if diff is positive
+        nextOffsetX = Math.floor(nextOffsetX / w) * w;
+      } else {
+        // swipe to the left if diff is negative
+        nextOffsetX = Math.ceil(nextOffsetX / w) * w;
+      }
+    } else {
+      // remain in the current image
+      nextOffsetX = Math.round(nextOffsetX / w) * w;
+    }
+
+    updateOffsetX(nextOffsetX);
+    setSwiping(false);
+
     window.removeEventListener("mousemove", swipeMove);
     window.removeEventListener("mouseup", swipeEnd);
     window.removeEventListener("touchmove", swipeMove);
@@ -82,6 +114,7 @@ export default function Home(props: HomeProps) {
         className="w-[100%] max-w-[100%] touch-pan-y overflow-hidden px-4"
       >
         <ul
+          ref={containerRef}
           className="w-max-[100%] duration-0.3 relative flex w-[100%] list-none flex-row transition ease-out"
           style={{
             transform: `translate3d(${offsetX}px, 0, 0)`,
