7bcf6faffd5aa4dc11272094fbdb24854a8f9573
Joseph van Bohemen <joevanbo@protonmail.com>
2023-05-24 22:26:12 +0100

    refactor no test

--- src/components/uploadCard/uploadCard.tsx
+++ src/components/uploadCard/uploadCard.tsx
@@ -12,11 +12,13 @@ import {
   uploadFile,
 } from "../../services/browser/photosLibraryService";
 import { updatePlant } from "../../services/browser/sheetsService";
+import { clearToken } from "../../services/browser/auth";
+import { useMutation } from "react-query";
 
 interface FormValues {
   water: boolean;
   food: boolean;
-  photo: File;
+  photo: File | null;
 }
 interface ImageProps {
   src: string;
@@ -28,26 +30,48 @@ export interface UploadCardProps {
   plant: Plant;
 }
 export const UploadCard: FC<UploadCardProps> = (props) => {
-  const [file, setFile] = useState<File | null>(null);
   const [image, setImage] = useState<ImageProps | null>();
-  const [loading, setLoading] = useState(false);
   const router = useRouter();
   const form = useForm<FormValues>();
 
-  const handleFileChange = async (files: FileList | null) => {
-    if (!files) {
+  const photo = form.watch("photo");
+
+  useEffect(() => {
+    if (photo) {
+      loadImage(photo);
+    }
+  }, [photo]);
+
+  const submitMutation = useMutation({
+    mutationFn: async (plant: Plant) => {
+      if (!photo) {
+        return;
+      }
+      const uploadResult = await uploadFile(plant, photo);
+      const mediaItem = await getMediaItem(uploadResult.id);
+      plant.imageSrc = mediaItem.baseUrl;
+      await updatePlant(plant);
+    },
+    onError: (error) => {
+      console.error(error);
+      console.error("submitMutation failed");
+      clearToken();
+    },
+    onSettled: () => {
+      router.push("/");
+    },
+  });
+
+  const handleFileChange = async (fileList: FileList | null) => {
+    if (!fileList) {
       return;
     }
-    const lastFile = files[files?.length - 1];
+    const lastFile = fileList[fileList.length - 1];
     form.setValue("photo", lastFile);
-    loadImage(lastFile);
   };
 
-  const loadImage = (file: File) => {
-    if (!file) {
-      return;
-    }
-    const url = URL.createObjectURL(file);
+  const loadImage = (f: File) => {
+    const url = URL.createObjectURL(f);
     const image = new window.Image();
     image.onload = () => {
       setImage({
@@ -56,35 +80,20 @@ export const UploadCard: FC<UploadCardProps> = (props) => {
         width: image.width,
         ts: Date.now(),
       });
-      setFile(file);
     };
     image.src = url;
   };
 
   const handleSubmit = async (values: FormValues) => {
-    if (!values.photo) {
-      return;
+    const nextPlant = { ...props.plant };
+    nextPlant.imageTS = image?.ts ?? Date.now();
+    if (values.food) {
+      nextPlant.lastFed = nextPlant.imageTS;
     }
-    setLoading(true);
-    try {
-      const nextPlant = { ...props.plant };
-      nextPlant.imageTS = image?.ts ?? Date.now();
-      if (values.food) {
-        nextPlant.lastFed = nextPlant.imageTS;
-      }
-      if (values.water) {
-        nextPlant.lastHydrated = nextPlant.imageTS;
-      }
-      // todo upload progress
-      const uploadResult = await uploadFile(props.plant, values.photo);
-      const mediaItem = await getMediaItem(uploadResult.id);
-      nextPlant.imageSrc = mediaItem.baseUrl;
-      await updatePlant(nextPlant);
-    } catch (e) {
-      console.error("http.uploadImage failed");
-      console.error(e);
+    if (values.water) {
+      nextPlant.lastHydrated = nextPlant.imageTS;
     }
-    setLoading(false);
+    await submitMutation.mutate(nextPlant);
   };
 
   const dateStr = image && toDateStr(new Date(image.ts));
@@ -144,8 +153,8 @@ export const UploadCard: FC<UploadCardProps> = (props) => {
             onChange={(e) => form.setValue("food", e.target.checked)}
           />
         </fieldset>
-        <button disabled={!file} type="submit">
-          {loading ? "Saving" : "Save"}
+        <button disabled={!photo} type="submit">
+          {submitMutation.isLoading ? "Saving" : "Save"}
         </button>
       </form>
     </div>
